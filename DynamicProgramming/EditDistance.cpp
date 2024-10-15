14bfe15d78c5e323ba33e281f82d85df65e8b9de
kiruthik5000 <kiruthikvairam5000@gmail.com>
2024-10-15 11:57:53 +0530

    on 15/10/2024

--- DynamicProgramming/EditDistance.cpp
+++ DynamicProgramming/EditDistance.cpp
@@ -1,32 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
-int fmin(int a, int b,int c){
-    return min(a, min(b, c));
-}
-int main() {
-    string str1, str2;
-    cin >> str1 >> str2;
+int finddist(string s1, string s2) {
+    int n = s1.length();
+    int m = s2.length();
 
-    int m = str1.length();
-    int n = str2.length();
+    vector<vector<int>> dp (n+1, vector<int> (m+1, 1));
 
-    vector<vector<int>> dp(m+1, vector<int> (n+1, 0));
-
-    for(int i=1; i<=m; i++) {
-        dp[i][0] = i; 
-    }
+    for(int i=0; i<n; i++) 
+        dp[i][0] = i+1;
+    for(int i=0; i<m; i++) 
+        dp[0][i] = i+1;
+    
     for(int i=1; i<=n; i++) {
-        dp[0][i] = i;
-    }
-
-    for(int i=1; i<=m; i++) {
-        for(int j=1; j<=n; j++) {
-            if(str1[i-1] == str2[j-1]) 
-                dp[i][j] = dp[i-1][j-1];
+        for(int j=1; j<=m; j++) {
+            if(s1[i - 1] == s2[j - 1])
+                dp[i][j] = dp[i - 1][j -1];
             else
-                dp[i][j] = fmin(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1;
+                dp[i][j] = min({dp[i][j-1], dp[i-1][j], dp[i-1][j-1]});
         }
     }
+    return dp[n][m];
+}
+int main() {
+    string s1, s2;
+    cin >> s1>> s2;
 
-    cout << dp[m][n];
+    cout<< finddist(s1, s2);
 }
---
+++ DynamicProgramming/MinCostPath.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int a; cin >> a;
+
+    int mat[a][a];
+
+    for(int i=0; i<a; i++)
+        for(int j=0; j<a; j++)
+            cin >> mat[i][j];
+    for(int i=1; i<a; i++) {
+        mat[i][0] += mat[i-1][0];
+    }
+    for(int i=1; i<a; i++) {
+        mat[0][i] += mat[0][i-1];
+    }
+    for(int i=1; i<a; i++)
+        for(int j=1; j<a; j++) 
+            mat[i][j] += min({mat[i-1][j-1], mat[i-1][j], mat[i][j-1]});
+
+    cout << mat[a-1][a-1];
+}
